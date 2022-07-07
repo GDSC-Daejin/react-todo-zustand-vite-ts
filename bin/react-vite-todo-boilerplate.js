37527757292019686a0e9bf985aa6ef7bff32658
HyeokE <jhjeong00@gmail.com>
2022-07-08 03:31:18 +0900

    chore: modify package.json

--- bin/react-vite-todo-boilerplate.js
+++ bin/react-vite-todo-boilerplate.js
@@ -13,7 +13,7 @@ if (process.argv.length < 3) {
 const projectName = process.argv[2];
 const currentPath = process.cwd();
 const projectPath = path.join(currentPath, projectName);
-const GIT_REPO = 'https://github.com/GDSC-Daejin/react-vite-todo-boilerplate';
+const GIT_REPO = 'https://github.com/GDSC-Daejin/react-vite-todo-boilerplate.git';
 
 if (projectName !== '.') {
   try {
